7f173f9eecf95c141292a6e793fb42d3425dfa96
muhammed <mbljstech@gmail.com>
2026-05-05 08:18:23 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // or whatever plugin you use
 
 export default defineConfig({
+  base: '/my-portfolio/',  // 👈 Add this line
   plugins: [react()],
-});
-
-export default {
-  base: '/portfolio/',  // 👈 must match your repo name exactly
-}
+})
